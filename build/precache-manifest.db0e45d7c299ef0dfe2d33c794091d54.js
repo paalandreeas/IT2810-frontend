0990d010e612b5648340c6a48951fe1c79758fb7
paalandreeas <pamorho@gmail.com>
2021-01-23 17:05:23 +0100

    Newest build

---
+++ build/precache-manifest.db0e45d7c299ef0dfe2d33c794091d54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b52464f984047ccc1075482817619a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b3addf38d92682e2aee",
+    "url": "/static/css/main.ce928496.chunk.css"
+  },
+  {
+    "revision": "def8acde59321da2b62f",
+    "url": "/static/js/2.207b6a7a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.207b6a7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b3addf38d92682e2aee",
+    "url": "/static/js/main.b4262aae.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
